246ce4155ddc528d4e8fadaf6d01784f39ac2766
josthynvaldez <157859189+josthynvaldez@users.noreply.github.com>
2024-01-26 20:20:33 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('no sabes cuanto me alegra que hayas escogido esta opcion. Te voy a querer aun mas de lo que ya lo hago ahora');
+
+} )
+
+
+const noBtn = document.querySelector('#noBtn');
+noBtn.addEventListener('click',function () {
+    alert('no existe');
+})
